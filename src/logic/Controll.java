4ff07e221209af0853ad4e4955b54fd39a4c3c93
DerEinherjer <steffen.wonner@web.de>
2016-08-21 14:55:54 +0200

    Teilnehmer werden eingetragen

--- src/logic/Controll.java
+++ src/logic/Controll.java
@@ -2,6 +2,8 @@ package logic;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -10,7 +12,6 @@ public class Controll
 	private static String url = "jdbc:h2:~/SprechfensterData";
 	
 	private Connection con = null;
-	private Statement stmt = null;
 	
 	public Controll()
 	{
@@ -19,7 +20,6 @@ public class Controll
 			Class.forName("org.h2.Driver");
 			
 			con = DriverManager.getConnection(url, "", "");
-			stmt = con.createStatement();
 		} 
 		catch (ClassNotFoundException e) 
 		{
@@ -35,14 +35,99 @@ public class Controll
 	
 	private void createTabels()
 	{
-		String sql = "CREATE TABELE Turniere (ID int,Name varchar(255))";
+		PreparedStatement stmt = null;
+		String sql = null;
 		try 
 		{
-			stmt.executeUpdate(sql);
+			sql = "CREATE TABELE Turniere (ID int,"
+					   + "Name varchar(255),"
+					   + "Datum varchar(11),"
+					   + "Gruppen int,"
+					   + "Finalrunden int,"
+					   + "Bahnen int);";
+			stmt = con.prepareStatement(sql);
+			stmt.executeQuery();
+		} 
+		catch (SQLException e) {}
+		
+		
+		try 
+		{
+			sql = "CREATE TABELE Fechter (ID int,"
+					   + "Vorname varchar(255),"
+					   + "Nachname varchar(255),"
+					   + "Geburtstag verchar(255),"
+					   + "Fechtschule varchar(255),"
+					   + "Nationalitaet varchar(255));";
+			stmt = con.prepareStatement(sql);
+			stmt.executeQuery();
+		} 
+		catch (SQLException e) {}
+		
+		try
+		{
+			sql = "CREATE TABLE Vorrunden( ID int,"
+										+ "TurnierID int,"
+										+ "Gruppe int,"
+										+ "Nummer int,"
+										+ "Teilnehmer1 int,"
+										+ "Teilnehmer2 int,)";
+			stmt = con.prepareStatement(sql);
+			stmt.executeQuery();
+		}
+		catch (SQLException e){}
+		
+		try
+		{
+			sql = "CREATE TABLE Finalrunden(ID int,"
+										 + "Teilnehmer1 int,"
+										 + "Teilnehmer2 int,"
+										 + "FolgeBegegnung int,"
+										 + "Vorbegegnung1 int,"
+										 + "Vorbegegnung2 int)";
+			stmt = con.prepareStatement(sql);
+			stmt.executeQuery();
+		}
+		catch (SQLException e){}
+		
+		try
+		{
+			sql = "CREATE TABLE Teilnahme(TurnierID int,"
+										 + "FechterID int,"
+										 + "Gruppe int)";
+			stmt = con.prepareStatement(sql);
+			stmt.executeQuery();
+		}
+		catch (SQLException e){}
+	}
+	
+	public Tournament createTournament(String Name, String Datum)
+	{
+		PreparedStatement stmt = null;
+		
+		String sql = "SELECT COUNT(*) FROM Turnire WHERE Name = ? OR Datum = ?;";
+		try 
+		{
+			stmt = con.prepareStatement(sql);
+			stmt.setString(0, Name);
+			stmt.setString(1, Datum);
+			ResultSet rs = stmt.executeQuery();
+			if(rs.getInt(0)<1)
+			{
+				sql = "INSER INTO Turniere (Name, Datum, Gruppen, Teilnehmer, Finalrunden, Bahnen)"
+						+ " VALUES (?, ?, 2, 8, 2, 2);";
+				
+			}
+			else
+			{
+				//Existiert bereits (Mit diesem Namen ODER an diesem Tag)
+			}
 		} 
 		catch (SQLException e) 
 		{
-			e.printStackTrace();
+			//Handle Exeption
 		}
+		
+		return null;
 	}
 }
--- src/logic/Tournament.java
+++ src/logic/Tournament.java
@@ -8,63 +8,39 @@ import java.sql.SQLException;
 public class Tournament 
 {
 	private int ID;
-	private Connection con;
+	private Connection con; //Static?
 	
 	protected Tournament(int ID, Connection con)
 	{
 		this.ID = ID;
 		this.con = con;
-		changeVorrunde = true;
-		changeFinalrunden = true;
 	}
 	
-	public void createFinalrunden()
+	public void addParticipant(Fencer fencer)
 	{
-		if(changeFinalrunden)
-		{
-			changeFinalrunden = false;
-		}
-	}
-	
-	public void createVorrunden()
-	{
-		if(changeVorrunde)
-		{
+		if(!isParticipant(fencer))
+		{//SELECT OrderID, COUNT(OrderID) AS Count FROM OrderDetails WHERE Quantity >=10 GROUP BY OrderID ORDER BY Count ASC, OrderID ASC;
+			int group = 1;
+			String sql = "SELECT Gruppe, COUNT(Gruppe) AS Count FROM Teilnahme WHERE TurnierID ="+ID+" GROUPE BY Gruppe ORDER BY Count ASC, Gruppe ASC;";
+			PreparedStatement stmt;
 			try 
 			{
-				changeVorrunde = false;
-				String sql = "DELETE FROM Vorrunden WHERE TurnierID ="+ID+";";
-				PreparedStatement stmt = con.prepareStatement(sql);
-				stmt.executeQuery();
-				
-				
-				
-				sql = "SELECT * FROM Turniere WHERE ID = "+ID+";";
-				
-				int teilnehmer = 0;
-				int gruppen = 0;
-			
-			
 				stmt = con.prepareStatement(sql);
 				ResultSet rs = stmt.executeQuery();
-				
-				teilnehmer = rs.getInt("Teilnehmer");
-				gruppen = rs.getInt("Gruppen");
-			
-			
-				int nummern [] = new int[gruppen];
-				
-				for (int i = 0; i<teilnehmer-1; i++)
+				group = rs.getInt("Gruppe"); 
+
+				boolean groups[] = new boolean[getGruppen()];
+				do
 				{
-					for(int c = i+1; c<teilnehmer; c++)
+					groups[rs.getInt("Gruppe")] = true;
+				}
+				while(rs.next());
+				for(int i=0;i<getGruppen();i++)
+				{
+					if(!groups[i])
 					{
-						if(i%gruppen == c%gruppen)
-						{
-							int gruppe = i%gruppen+1;
-							sql = "INSERT INTO Vorrunden (TurnierID, Gruppe, Nummer, Teilnehmer1, Teilnehmer2)"
-									+ "VALUES ("+ID+", "+gruppe+", "+(++nummern[gruppe])+", "+i+", "+c+");";
-							stmt = con.prepareStatement(sql);
-						}
+						group = i+1;//Konvertierung von Index zu "Name"
+						break;
 					}
 				}
 			} 
@@ -72,6 +48,121 @@ public class Tournament
 			{
 				e.printStackTrace();
 			}
+			sql = "INSER INTO Teilnehmer (TurnierID, FechterID, Gruppe) VALUES ("+ID+", "+fencer.getID()+", "+group+");";
+			try 
+			{
+				stmt = con.prepareStatement(sql);
+				stmt.executeUpdate();
+			} 
+			catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
 		}
+		createVorrundenBegegnungen(fencer);
+	}
+	
+	public void addParticipant(Fencer fencer, int group)
+	{
+		String sql = "";
+		int fencerID = fencer.getID();
+		PreparedStatement stmt = null;
+		int groups = getGruppen();
+		
+		if(group<1 || group>groups)
+				return;//Keine Gülltige gruppe;
+		
+		if(isParticipant(fencer))
+		{
+			if(getGroupNummer(fencer)==group)
+				return; //Fechter ist bereits in dieser Gruppe
+			
+			sql = "DELETE FROM Vorrunden WHERE TurnierID = "+ID+" AND (Teilnehmer1 = "+fencerID+" OR "
+					                                                 +"Teilnehmer2 = "+fencerID+");";
+			try 
+			{
+				stmt= con.prepareStatement(sql);
+				stmt.executeUpdate();
+				
+				sql = "UPDATE Teilnahme SET Gruppe = "+group+" WHERE TurnierID = "+ID+" AND FechterID = "+fencerID+";";
+				stmt = con.prepareStatement(sql);
+				stmt.executeUpdate();
+			} 
+			catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+			
+		}
+		else
+		{
+			sql = "INSER INTO Teilnehmer (TurnierID, FechterID, Gruppe) VALUES ("+ID+", "+fencer.getID()+", "+group+");";
+			try 
+			{
+				stmt = con.prepareStatement(sql);
+				stmt.executeUpdate();
+			} 
+			catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		createVorrundenBegegnungen(fencer);
+	}
+	
+	public boolean isParticipant(Fencer fencer)
+	{
+		String sql = "SELECT COUNT(*) FROM Teilnahme WHERE Turniere = "+ID+" AND Fechter = "+fencer.getID()+";";
+		PreparedStatement stmt;
+		try 
+		{
+			stmt = con.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			if(rs.getInt(0)==0)
+				return false;
+			else
+				return true;
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+	
+	public int getGroupNummer(Fencer fencer)
+	{
+		String sql = "SELECT Group FROM Teilnahme WHERE Turniere = "+ID+" AND Fechter = "+fencer.getID()+";";
+		PreparedStatement stmt;
+		try 
+		{
+			stmt = con.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			return rs.getInt("Gruppe");
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		return -1;
+	}
+	
+	public int getGruppen()
+	{
+		String sql = "SELECT Gruppen FROM Turniere WHERE ID ="+ID+";";
+		try
+		{
+			PreparedStatement stmt = con.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			return rs.getInt("Gruppen");
+		}
+		catch(SQLException e){}
+		return -1;
+	}
+	
+	private void createVorrundenBegegnungen(Fencer fencer)
+	{
+		
 	}
 }
